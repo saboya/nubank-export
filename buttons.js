38124fc939df0387842ff65ae0cdc91f0a008e53
Rodrigo Saboya <saboya@users.noreply.github.com>
2016-04-11 21:51:30 -0300

    Colocando quotes em todos os campos. Adicionando exportação CSV com tags. Refactor nos tipos de exportação, simplificando o código.

--- buttons.js
+++ buttons.js
@@ -12,26 +12,52 @@ function add_buttons() {
 	for (var i = billSummaries.length-1; i >= 0; i--) {
 		for (let name of Object.keys(filters)) {
 			var button = document.createElement("button");
-			button.setAttribute('data-exportfilter',name);
+			button.setAttribute('data-exporttype',name);
 			button.classList.add('nu-button','stroke');
-			button.innerHTML = 'Exportar YNAB4';
+			button.innerHTML = 'Exportar '+name;
 			billSummaries[i].appendChild(button);
 		}
 	}
 }
 
-window.addEventListener('click',function(event) {
-	if(event.target.hasAttribute('data-exportfilter')) {
-		var bill_index = parseInt(event.target.closest('div.md-tab-content').getAttribute('aria-labelledby').split('_')[1],16);
+window.addEventListener('click',(clickevent) => {
+	if(clickevent.target.hasAttribute('data-exporttype')) {
+		var bill_index = parseInt(clickevent.target.closest('div.md-tab-content').getAttribute('aria-labelledby').split('_')[1],16);
 		var bill = _script_data.bills[_script_data.bills.length - bill_index];
-		var data = filters[event.target.getAttribute('data-exportfilter')](bill);
+		var events = _script_data.events;
+		var exporttype = filters[clickevent.target.getAttribute('data-exporttype')];
+
+		var data = exporttype.pre();
+
+		for(let entry of bill.more.line_items) {
+			if(typeof entry.id != 'undefined') {
+				var extras;
+				var item = {
+					date: entry.post_date,
+					title: entry.title,
+					amount: entry.amount/100
+				};
+				if(typeof entry.href != 'undefined') {
+					var event = events[entry.href.split('/').slice(-1)[0]];
+					if(typeof event != 'undefined') {
+						extras = {
+							category: event.title,
+							tags: event.details.tags,
+							card_present: event.details.subcategory == "card_present"
+						};
+					}
+				}
+
+				data += exporttype.entry(item,extras);
+			}
+		}
 
 		var blob = new Blob([data], {type: "text/plain;charset=utf-8"});
-		saveAs(blob,'nubank-'+bill.summary.due_date+'.csv',false);
+		saveAs(blob,'nubank-'+bill.summary.due_date+'-'+clickevent.target.getAttribute('data-exporttype')+'.csv',false);
 	}
 });
 
-window.addEventListener("message", function(event) {
+window.addEventListener("message",(event) => {
 	// We only accept messages from ourselves
 	if (event.source != window) {
 		return;
--- main-script.js
+++ main-script.js
@@ -1,28 +1,39 @@
 "use strict";
 
-var filters = {
-	'YNAB4': function(bill) {
-		// Date,Payee,Category,Memo,Outflow,Inflow
-		var lines = [];
-		lines.push("Date,Payee,Category,Memo,Outflow,Inflow");
-		for(let entry of bill.more.line_items) {
-			if(typeof entry.id != 'undefined') {
-				var date = '"'+entry.post_date+'"';
-				var payee = '"'+entry.title+'"';
-				var category = '';
-				var memo = '';
-				var outflow = '';
-				var inflow = '';
-				if(entry.amount >= 0) {
-					outflow = entry.amount/100;
-				}
-				else {
-					inflow = -entry.amount/100;
-				}
-				lines.push([date,payee,category,memo,outflow,inflow].join(','));
-			}
-		}
 
-		return lines.join("\n");
+var filters = {
+	'CSV': {
+		_delim: ',',
+		_tags_delim: ';',
+		_quote: '"',
+		pre: (separator) => {
+			return ["Date","Payee","Category","Tags","Amount"].join(filters['CSV']._delim)+"\n";
+		},
+		entry: (item,extra) => {
+			return [
+				item.date,
+				item.title,
+				extra ? extra.category : '',
+				extra ? extra.tags.join(filters['CSV']._tags_delim) : '',
+				item.amount
+			].map((value) => { return filters['CSV']._quote+value+filters['CSV']._quote}).join(filters['CSV']._delim)+"\n";
+		}
+	},
+	'YNAB4': {
+		_delim: ',',
+		_quote: '"',
+		pre: (separator) => {
+			return ["Date","Payee","Category","Memo","Outflow","Inflow"].join(filters['YNAB4']._delim)+"\n";
+		},
+		entry: (item,extra) => {
+			return [
+				item.date,
+				item.title,
+				extra ? extra.category : '',
+				'',
+				item.amount >= 0 ? item.amount : '',
+				item.amount < 0 ? -item.amount : ''
+			].map((value) => { return filters['YNAB4']._quote+value+filters['YNAB4']._quote}).join(filters['YNAB4']._delim)+"\n";
+		}
 	}
 }
